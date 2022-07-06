1808723dc289af59e718914efa04ce55928729fe
code-moro <mknp86.5@gmail.com>
2022-07-06 16:09:35 +0530

    Modified Array_Sum.go

--- Array_Sum.go
+++ Array_Sum.go
@@ -1,36 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
 
 
-func routineone(arr []int,ch chan int,wg *sync.WaitGroup){
-	slice:=arr[:51]
+func routineone(arr []int,ch chan int,a,b int){
 	sum:=0
-	for i:=0;i<len(slice);i++{
-     sum=sum+slice[i]
+	for i:=a;i<b;i++{
+     sum=sum+arr[i]
 	}
 	ch<-sum
-	wg.Done()
-} 
-
-func routinetwo(arr []int,ch chan int,wg *sync.WaitGroup){
-	slice:=arr[50:]
-	sum:=0
-	for i:=0;i<len(slice);i++{
-     sum=sum+slice[i]
-	}
-	ch<-sum
-	wg.Done()
 } 
 
 func main(){
-	wg := new(sync.WaitGroup)
 
-	wg.Add(2)
     // unbuffered channel created 
 
 	ch:=make(chan int)
@@ -41,16 +24,16 @@ func main(){
 		arr[i]=i
 	}
 
-	// call for 1st go routine to calculate sum of 1st 50 numbers
-    go routineone(arr,ch,wg)
+
+    go routineone(arr,ch,0,50)
 
 	fmt.Println("sum of 0 to 50 range numbers",<-ch)
-	// call for 1st go routine to calculate sum of 1st 50 numbers
+	
 
-	go routinetwo(arr,ch,wg)
+	go routineone(arr,ch,51,100)
 
 	fmt.Println("sum of 51 to 100 range numbers",<-ch)
+	close(ch)
     
-	wg.Wait()
 
 }
